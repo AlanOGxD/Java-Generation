a7f2c06235440f1cbfe5662f8632800a0dbbfa7d
AlanOGxD <alanosvaldo88@gmail.com>
2023-10-04 01:00:10 -0600

    Se añade sesion POO paciente

--- POO/src/Persona/Test.java
+++ POO/src/Persona/Test.java
@@ -1,13 +1,43 @@
-package Persona;
+package POO;
 
 public class Test {
-
+	
+	//Método principal para instanciar mis clases
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Persona Felipe = new Persona("Felipe", "Maqueda", (byte) 31, "5544564654", "Felipe@correo");
-
+		
+		//Instancia de Persona
+		Persona Felipe = new Persona("Felipe", "Maqueda", (byte)31, "5512345678", "felipe@mail.com");
+		System.out.println(Felipe);
 		Felipe.imprimirInfo();
-	}
+		Felipe.saludar();
+		
+		
+		System.out.println("**********************");
+		
+		
+		//Instancia de Dentista
+		Dentista Simi = new Dentista("Dr. Simi", "Lares", (byte)50, "1234567890", "simi@lares.com", "Odontología", "1544385", "Consultorio B5", "avanzado", "matutino", 500);
+		Simi.calcularSalario();
+		Simi.imprimirInfoDentista();
+		
+		
+		System.out.println("**********************");
+		
+		
+		//Instancia de Paciente con todos los datos
+		Paciente Chencho = new Paciente("CHNCH1234", "RH Positivo", true, true, "05/Oct/2023", "Doc. Simi Lares", false);
+		System.out.println(Chencho);
+		
+		
+		//instancia de Pacientes con datos obligatorios
+		Paciente Masiosare = new Paciente("MAS12345", false, false);
+		System.out.println(Masiosare);
+		
+		//Cambiar el false de seguroMedico a true
+//		Masiosare.seguroGastosMedicos = true;
+		System.out.println(Masiosare);
+		
+		
+	}//Cierre método main
 
-}
+}//Cierre class persona
