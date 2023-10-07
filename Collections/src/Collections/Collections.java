a1b7183ca3dc8c8a5b2a1879cb708139c25cf193
AlanOGxD <alanosvaldo88@gmail.com>
2023-10-06 20:37:32 -0600

    Añadiendo correciones y actualizaciones

--- Collections/src/Collections/Collections.java
+++ Collections/src/Collections/Collections.java
@@ -1,55 +1,129 @@
 package Collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Collections {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Array
-		//tipo de datos del array, nombre, instancia del objeto, arreglo, los datos
-		String[] listaDeCompras = new String[] {
-				"Leche", "Pan", "Huevos"
-				};
+		 
+		 //Array
+		 //Tipo de datos del arary, nombre, instancia del objeto arreglo, los datos
+		 String [] listaDeCompras = new String [] {"Leche", "Pan", "Huevos"};
+		 
+		 //Esta impresion solo muestra el espacio de memoria
+		 System.out.println(listaDeCompras); 
+		 
+		 //Impresion info de listaDeCompras con un forEach (Para el dato de tipo string que llamaremos productoAImprimir de la lista de compras, lo camos a tomar y lo vamos a imprimir
+		// for (String productoAImprimir : listaDeCompras) {
+		//	 System.out.println(productoAImprimir);
+		// }
+		 
+		 //Implementacionde un arrayList
+		 //Clase General ( interfaz), tipo de dato que usare, nombre de mi estructura de datos, instancia de dicha clase general
+		 List<String> listadeContactos = new ArrayList<>(); 
+		 
+		 //Agrego elementos del tipo String a mi arrayList llamado listaDeContactos
+		 listadeContactos.add("Felipe");
+		 listadeContactos.add("Roberto");
+		 listadeContactos.add("Alicia");
+		 listadeContactos.add("Fatima");
+
+		 //Obtener uyn elemento de la lista
+		 String datoFelipe = listadeContactos.get(0);
+		 System.out.println(datoFelipe);
+		 
+		 //Borrar un elemento de la lista
+		 listadeContactos.remove(1);
+		 
+		 System.out.println(listadeContactos); 
+		 
+		 //Implementacion de un conjunto (SET)
+		 //Sintaxis
+		 //interfase de donde implemento + tipo de dato que usare + nombre que le doy al conjunto + instancia de la clase especifica
+		 
+		 Set<String> cartasDeJuego = new HashSet<>() ;
+		 //Agregar elementos
+		 cartasDeJuego.add("As de Corazonez");
+		 cartasDeJuego.add("2 de picas");
+		 cartasDeJuego.add("Reina de Treboles");
+
+		 //No puedo saber posiciones, solo saber si lo contiene
+		 if(cartasDeJuego.contains ("Reina de Corazonez")) {
+			 System.out.println("La reina de treboles si esta en el conjunto");
+		 }else {
+			 System.out.println("No encuentro el elemento solicitado");
+		 }
+		 
 		
-		//Esta impresión solo muestra el espacio de memoria
-		System.out.println(listaDeCompras);
-		
-		//Imprimir info de listaDeCompras con un forEach
-		//Para el dato de tipo String que llamaremos productoAImprimir de la lista de compras, lo valos a tomar y lo vamos a imprimir
-		for(String productoAImprimir : listaDeCompras) {
-			System.out.println(productoAImprimir);
-		}
-		
-		
-		
-		
+		 //Implementacion de un mapa (Hash map)
+		 Map<String, String> libretaDirecciones = new HashMap<>();
+		 
+		 //Agregar elementos a mi libreta de direcciones, donde mi K=nombre y mi v=direccion
+		 libretaDirecciones.put("Felipe", "Tlalnepantla");
+		 libretaDirecciones.put("Doctor", "CDMX");
+		 libretaDirecciones.put("Konoha", "Naruto");
+		 
+		 
+		 System.out.println(libretaDirecciones.get("Doctor"));
+		 
 	}
 
 }
-
-
 /*
- * Colecciones
- * 
- * 	-Arrays (arreglos)
- * 
- * 	Es una estructura de datos olineal que contiene elementos del mismo tipo. Los elementos están almacenados de forma contigua (uno al lado del otro), y se pueden acceder a ellos a través de un índice. Las principales características de un array son:
- * 
- * 		- Es una estructura de datos fija, generalmente almacena datos del mismo tipo.
- * 		- La longitud de un array se establece al momento e crearlo, y no cambia.
- * 		- Podemos acceder a elementos per medio de un índice.
- * 		- Los arrays pueden contener datos primitivos y objetos
- * 		- No proporciona métodos adicionales para agregar o eliminar elementos
+ * Colecciones 
  * 
  * 
- * Un ejemplo de array es una lista de compras del supermercado
- */
-/*- ArrayList
  * 
  * 
- * Es una implementacion de una lista (List), es decir, que esta en un nivel abajo de las listas. Utiliza un array dinamico que permite almacenar datos.
+ * -Arrays (arreglos)
  * 
- * Sus principales caracteristicas son:
+ * Es una estructura de datos lineal que contiene elementos del mismo tipo. Los elementos estan almacenados de forma contigua (uno al lado de otro), y se puede aaceder a ellos a traves de un indice. Las principales caracteristicas de un array son: 
+ * -Es una estructura de datos fija, generalmente almacena datos del mismo tipo.
+ * -La longitud de un array se establece al momento de crearlo, y no lo cambia.
+ * -Podemos acceder a elementos por medio de un indice.
+ * -Los arrays pueden contener datos primitivos y objetos. 
+ * -No proporciona metodos adicionales para agregar o eliminar elementos. 
  * 
- * 	- Tamanio dinamico, a diferencia de los arrays tradicionales, el tamanio de un arrayList si se puede modificar.
- *  - Acceso rapido, podemos acceder a un elemento del arrayList por su posicion o indice
- *  - Coste elevado al momento de insertar o eliminar elementos. Si agrego un elemento, tengo que recorrer todos los demas elementos.*/
+ * Un ejemplo de array es una lista de compras del supermercado.
+ * 
+ * 
+ * -Array List
+ * Es una implementacion de una lista(list), es decir que esta en un nivel abajo de las listas. Utiliza un array dinámico que permite almacenar datos.
+ * 
+ * Sus principales caracteristicas son: 
+ * -Tamaño dinámico, a diferencia de los arrays tradicionales, el tamaño de una arrayList si se puede modificar.
+ * -Acceso rapido, podemos acceder a un elemento del arrayList por su posicion o indice
+ * -Coste elevado al momento de insertar o eliminar elementos. Si agrego un elemento, tengo que recorrer todos los demas elementos. 
+ * 
+ * 
+ * 
+ * Set (conjunto)
+ * Estructura de datos que almacena datos unicos sin orden especifico. Las principales caracteristicas de un set son:
+ * -No hay elementos duplicados, ya que los conjuntos no los pueden contener. Si se intenta agregar un elemento ya existente, la operacion se ignora.
+ * -No hay una orden especifico, los elementos se almacenan sin orden especifico
+ * -La busqueda es mas sencilla, ya que al no tener elementos duplicados se usa una funcion hash para encontrar elemento deseado. 
+ * 
+ * 
+ * 
+ * 
+ * Map(mapas)
+ * 
+ * Un mapa es una estructura de datos que almacena pares de clave-valor.
+ * 
+ *  - Almacenamiento con pares K-V
+ *  - Sin orden especifico (similar a un set)
+ *  - Busqueda rapida, la busqueda se hace por medio de una valor (key), y usa una funcion interna hash para buscar elementos.
+ *  
+ *  
+ *  
+ *  Agenda telefonica (agregar, modificar, eliminar)
+ *  
+ *  
+ *  
+ * 
+ * */
--- POO/src/Persona/Test.java
+++ POO/src/Persona/Test.java
@@ -1,5 +1,12 @@
 package Persona;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Test {
 	
 	//Método principal para instanciar mis clases
@@ -36,6 +43,37 @@ public class Test {
 		//Cambiar el false de seguroMedico a true
 //		Masiosare.seguroGastosMedicos = true;
 		System.out.println(Masiosare);
+		
+		
+		
+		//implementacion de Arraylist de Dentistas
+		List<Dentista> listaDentistaArrayList = new ArrayList<>();
+		
+		Dentista chapatin = new Dentista("Dr chapatin", "Chespirito", (byte)45, "1321321", "correo", "Odontologia", "55465464", "Consultorio 500", "Avanzado", "Matutino", 500);
+		
+		//Hago una instancia y la agrego directamente a la lista
+		listaDentistaArrayList.add(new Dentista("Dr Simi", "Lares", (byte)45, "1321321", "correo", "Odontologia", "55465464", "Consultorio 500", "Avanzado", "Matutino", 500));
+		
+		
+		//Implementacion de la lista de Dentistas con un HashSet
+		Set<Dentista> conjuntoDentistas = new HashSet<>();
+		
+		
+		//Agregando Dentista a mi Connjunto
+		conjuntoDentistas.add(new Dentista("Dr chapatin", "Chespirito", (byte)45, "1321321", "correo", "Odontologia", "55465464", "Consultorio 500", "Avanzado", "Matutino", 500));
+		
+		
+		
+		
+		//Implementacion de un hashmap de Dentistas
+		Map<String, Dentista> mapaDentistas = new HashMap<>();
+		
+		//Agregar un dentista a mi mapa
+		mapaDentistas.put("Dr House", new Dentista("Gregory", "House", (byte)45, "1321321", "correo", "Odontologia", "55465464", "Consultorio 500", "Avanzado", "Matutino", 1400));
+		
+		
+		
+		
 		
 		
 	}//Cierre método main
