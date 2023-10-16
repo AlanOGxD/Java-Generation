58ccae2b175cf4bb81297502657bff5c63d5122e
AlanOGxD <alanosvaldo88@gmail.com>
2023-10-15 20:45:42 -0600

    añadiendo archivos

---
+++ Figuras/src/Figura/Calculable.java
@@ -0,0 +1,12 @@
+package Figura;
+
+public interface Calculable {
+	
+	//Area, olumen, Perimetro
+	
+	//Metodos de la interface
+	double calcularArea();
+	double calcularVolumen();
+	double calcularPerimetro();
+
+}//interface Calculable
---
+++ Figuras/src/Figura/Esfera.java
@@ -0,0 +1,29 @@
+package Figura;
+
+//clase concreta que implementa metodos de una interfaz llamada calculable
+public class Esfera implements Calculable{
+
+	//Atributos
+	double r, pi=3.1415;
+	
+	@Override
+	public double calcularArea() {
+		// TODO Auto-generated method st
+		return 0;
+	}
+
+	@Override
+	public double calcularVolumen() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double calcularPerimetro() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+}//Class esfera
